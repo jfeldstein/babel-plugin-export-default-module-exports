e70a06b5b872bbe82d3cf515b56c1c1bc62f33ea
Jordan Feldstein <jfeldstein@gmail.com>
2016-07-02 11:16:18 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+module.exports = ({types}) => ({
+  visitor: {
+    Program: {
+      exit (path) {
+        if (path.BABEL_PLUGIN_EXPORT_DEFAULT_MODULE_EXPORTS) {
+          return
+        }
+
+        let hasExportDefault = false
+        let hasExportNamed = false
+        path.get('body').forEach((path) => {
+          if (path.isExportDefaultDeclaration()) {
+            hasExportDefault = true
+            return
+          }
+
+          if (path.isExportNamedDeclaration()) {
+            if (path.node.specifiers.length === 1 && path.node.specifiers[0].exported.name === 'default') {
+              hasExportDefault = true
+            } else {
+              hasExportNamed = true
+            }
+            return
+          }
+        })
+
+        if (!hasExportDefault && hasExportNamed) {
+          path.pushContainer('body', [
+            types.expressionStatement(types.assignmentExpression(
+              '=',
+              types.memberExpression(types.identifier('exports'), types.stringLiteral('default')),
+              types.memberExpression(types.identifier('module'), types.identifier('exports'), true)
+            ))
+          ])
+        }
+
+        path.BABEL_PLUGIN_EXPORT_DEFAULT_MODULE_EXPORTS = true
+      }
+    }
+  }
+})
